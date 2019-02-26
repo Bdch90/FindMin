8baee346aad391ac7d629ecea36b4c5c75865891
Bdch90 <46187905+Bdch90@users.noreply.github.com>
2019-02-26 07:55:21 +0700

    Update FindMin.java

--- FindMin.java
+++ FindMin.java
@@ -1,8 +1,17 @@
-public class FindMin 
+import java.util.Scanner;
+
+public class FindMin
 {
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        int[] array ={1,2,5,0,9};
+        int[] array = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập từng phần tử vào mảng , tối đa 10 phần tử:");
+        for (int x = 0; x < array.length; x++)
+        {
+            System.out.println("phần tử thứ " + (x + 1) + ":");
+            array[x] = scanner.nextInt();
+        }
         int index = findMin(array);
         System.out.println("Min : " + array[index] + " at posotion " + index);
     }
